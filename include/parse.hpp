1b896a3e730d4957b43d1e40536696e37d9efe38
dmitrydi <d.dmitrook@gmail.com>
2025-10-28 16:22:41 +0000

    fixed devcontainer
    implemented
    
    fixed errors

--- include/parse.hpp
+++ include/parse.hpp
@@ -1,21 +1,112 @@
 #pragma once
 
+#include "types.hpp"
+#include <algorithm>
+#include <charconv>
 #include <expected>
+#include <format>
+#include <source_location>
 #include <string>
 #include <string_view>
+#include <system_error>
+#include <type_traits>
+#include <unordered_set>
 #include <utility>
 #include <vector>
-
-#include "types.hpp"
 
 namespace stdx::details {
 
 // здесь ваш код
+template <typename T>
+std::expected<T, scan_error> do_parse(std::string_view input);
+
+template <typename T>
+concept IsValue = !std::is_pointer_v<T> && !std::is_reference_v<T>;
+
+template <typename T>
+concept UnsignedFixedIntegral =
+    std::is_same_v<std::remove_cv_t<T>, uint8_t> || std::is_same_v<std::remove_cv_t<T>, uint16_t> ||
+    std::is_same_v<std::remove_cv_t<T>, uint32_t> || std::is_same_v<std::remove_cv_t<T>, uint64_t>;
+
+template <typename T>
+concept SignedFixedIntegral =
+    std::is_same_v<std::remove_cv_t<T>, int8_t> || std::is_same_v<std::remove_cv_t<T>, int16_t> ||
+    std::is_same_v<std::remove_cv_t<T>, int32_t> || std::is_same_v<std::remove_cv_t<T>, int64_t>;
+
+template <typename T>
+concept FixedIntegral = SignedFixedIntegral<T> || UnsignedFixedIntegral<T>;
+
+template <typename T>
+concept FloatingNumber = std::is_same_v<std::remove_cv_t<T>, float> || std::is_same_v<std::remove_cv_t<T>, double>;
+
+template <typename T>
+concept Number = FixedIntegral<T> || FloatingNumber<T>;
+
+template <Number T>
+std::expected<T, scan_error> do_parse(std::string_view input) {
+    T val;
+    if (auto result = std::from_chars(input.begin(), input.end(), val);
+        result.ec == std::errc{} && result.ptr == input.data() + input.size()) {
+        return val;
+    }
+    return std::unexpected(scan_error(std::format("could not convert {}", input)));
+}
+
+template <>
+inline std::expected<std::string_view, scan_error> do_parse(std::string_view input) {
+    return input;
+}
+
+template <>
+inline std::expected<std::string, scan_error> do_parse(std::string_view input) {
+    return std::string(input);
+}
+
+static const std::unordered_set<char> kFromats = {'d', 's', 'u', 'f'};
 
 // Функция для парсинга значения с учетом спецификатора формата
 template <typename T>
 std::expected<T, scan_error> parse_value_with_format(std::string_view input, std::string_view fmt) {
     // здесь ваш код
+    if (!IsValue<T>) {
+        return std::unexpected(scan_error("pointers and references are forbidden"));
+    }
+    if (fmt.empty()) {
+        return do_parse<T>(input);
+    }
+    if (fmt.size() != 2) {
+        return std::unexpected(scan_error("invalid format"));
+    }
+    auto f = fmt[1];
+    if (!kFromats.count(f)) {
+        return std::unexpected(scan_error(std::format("unknown format specifier", fmt)));
+    }
+    switch (f) {
+    case 'd':
+        if (!FixedIntegral<T>) {
+            return std::unexpected(scan_error(std::format("%d format specified but template type is not integral")));
+        }
+        break;
+    case 's':
+        if (!std::is_same_v<T, std::string>) {
+            return std::unexpected(scan_error(std::format("%s format specified but template type is not std::string")));
+        }
+        break;
+    case 'u':
+        if (!UnsignedFixedIntegral<T>) {
+            return std::unexpected(
+                scan_error(std::format("%u format specified but template type is not unsigned integral")));
+        }
+        break;
+    case 'f':
+        if (!FloatingNumber<T>) {
+            return std::unexpected(scan_error(std::format("%f format specified but template type is not float")));
+        }
+        break;
+    default:
+        return std::unexpected(scan_error(std::format("unknown format specifier", fmt)));
+    }
+    return do_parse<T>(input);
 }
 
 // Функция для проверки корректности входных данных и выделения из обеих строк интересующих данных для парсинга
@@ -70,4 +161,4 @@ parse_sources(std::string_view input, std::string_view format) {
     return std::pair{format_parts, input_parts};
 }
 
-} // namespace stdx::details
+}  // namespace stdx::details
--- tests/scan_test.cpp
+++ tests/scan_test.cpp
@@ -1,9 +1,57 @@
-#include <gtest/gtest.h>
-#include <print>
-
+#include "parse.hpp"
 #include "scan.hpp"
+#include "types.hpp"
+#include <gtest/gtest.h>
+#include <iostream>
+
+TEST(ScanTest, Parse) {
+    auto input = std::string_view("I want to sum 42 and 3.14 numbers.");
+    auto format = std::string_view("I want to sum {} and {%f} numbers.");
+    auto res = stdx::details::parse_sources<>(input, format);
+    ASSERT_TRUE(res.has_value());
+}
 
 TEST(ScanTest, SimpleTest) {
     auto result = stdx::scan<std::string>("number", "{}");
+    ASSERT_TRUE(result);
+    ASSERT_EQ(std::get<0>(result->data), std::string("number"));
+}
+
+TEST(ScanTest, TestTwoNumbers) {
+    auto result = stdx::scan<int, double>("I want to sum 42 and 3.14 numbers.", "I want to sum {} and {%f} numbers.");
+    ASSERT_TRUE(result);
+    ASSERT_EQ(std::get<0>(result->data), int(42));
+    ASSERT_EQ(std::get<1>(result->data), double(3.14));
+}
+
+TEST(ScanTest, TestTwoNumbersFail) {
+    auto result = stdx::scan<int, double>("I want to sum 4s2 and 3.14 numbers.", "I want to sum {} and {%f} numbers.");
+    ASSERT_FALSE(result);
+}
+
+TEST(ScanTest, TestInvalidType) {
+    auto result =
+        stdx::scan<int, std::string>("I want to sum 42 and 3.14 numbers.", "I want to sum {} and {%f} numbers.");
+    ASSERT_FALSE(result);
+}
+
+TEST(ScanTest, TestInvalidIntegerType) {
+    auto result = stdx::scan<int, float>("I want to sum 42 and 3.14 numbers.", "I want to sum {%u} and {%f} numbers.");
+    ASSERT_FALSE(result);
+}
+
+TEST(ScanTest, TestInvalidFloatingType) {
+    auto result = stdx::scan<int, float>("I want to sum 42 and 3.14 numbers.", "I want to sum {%d} and {%d} numbers.");
+    ASSERT_FALSE(result);
+}
+
+TEST(ScanTest, TestSignedOK) {
+    auto result = stdx::scan<int>("I want to get -42.", "I want to get {%d}.");
+    ASSERT_TRUE(result);
+    ASSERT_EQ(result->value<0>(), -42);
+}
+
+TEST(ScanTest, TestUnisgned) {
+    auto result = stdx::scan<unsigned>("I want to get -42.", "I want to get {%u}.");
     ASSERT_FALSE(result);
 }
